b5f31a7b02f5453be61d96efdb22520356e7daee
Saw2110 <sabin.ghimire1827@gmail.com>
2024-06-11 11:30:33 +0545

    latest

--- lib/screen/orglist/orglist_screen.dart
+++ lib/screen/orglist/orglist_screen.dart
@@ -49,126 +49,126 @@ class _OrgListScreenState extends State<OrgListScreen> {
           ]),
           body: state.dataList.isNotEmpty
               ? ListView.builder(
-              itemCount: state.dataList.length,
-              itemBuilder: (context, index) {
-                return Dismissible(
-                    key: UniqueKey(),
-                    onDismissed: (direction) {
-                      ConfirmationAlert.confirmation(
-                          text: "you want to delete data ?",
-                          context: context,
-                          onCancel: () {
-                            state.getDataFromDB();
-                          },
-                          onConfirm: () {
-                            state.deleteById(
-                              idValue: state.dataList[index].primaryId!,
-                            );
-                          });
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 10.0, vertical: 2.0),
-                      padding: const EdgeInsets.all(10.0),
-                      decoration: ContainerDecoration.decoration(),
-                      child: InkWell(
-                        onTap: () {
-                          state.dataValue = state.filterList(
-                            orgName: state.dataList[index].orgName,
-                          );
+                  itemCount: state.dataList.length,
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                        key: UniqueKey(),
+                        onDismissed: (direction) {
+                          ConfirmationAlert.confirmation(
+                              text: "you want to delete data ?",
+                              context: context,
+                              onCancel: () {
+                                state.getDataFromDB();
+                              },
+                              onConfirm: () {
+                                state.deleteById(
+                                  idValue: state.dataList[index].primaryId!,
+                                );
+                              });
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 2.0),
+                          padding: const EdgeInsets.all(10.0),
+                          decoration: ContainerDecoration.decoration(),
+                          child: InkWell(
+                            onTap: () {
+                              state.dataValue = state.filterList(
+                                orgName: state.dataList[index].orgName,
+                              );
 
                               Navigator.pushNamed(
-                            context,
-                            orgDetailPath,
-                            arguments: state.dataList[index].orgName,
-                          );
-                        },
-                        child: Row(children: [
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment:
-                                CrossAxisAlignment.start,
-                                children: [
-                                  RichText(
-                                      text: TextSpan(
-                                          text:
-                                          state.dataList[index].orgName,
-                                          style: TextStyle(
-                                              fontSize: 15.0,
-                                              color: primaryColor,
-                                              fontWeight: FontWeight.bold,
-                                              decoration:
-                                              TextDecoration.underline),
-                                          children: [
+                                context,
+                                orgDetailPath,
+                                arguments: state.dataList[index].orgName,
+                              );
+                            },
+                            child: Row(children: [
+                              Expanded(
+                                flex: 2,
+                                child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      RichText(
+                                          text: TextSpan(
+                                              text:
+                                                  state.dataList[index].orgName,
+                                              style: TextStyle(
+                                                  fontSize: 15.0,
+                                                  color: primaryColor,
+                                                  fontWeight: FontWeight.bold,
+                                                  decoration:
+                                                      TextDecoration.underline),
+                                              children: [
                                             TextSpan(
                                                 text:
-                                                ' -(${state.dataList[index].orgAddress})',
+                                                    ' -(${state.dataList[index].orgAddress})',
                                                 style: const TextStyle(
                                                   fontSize: 13.0,
                                                   color: Colors.black,
                                                   decoration:
-                                                  TextDecoration.none,
+                                                      TextDecoration.none,
                                                 ))
                                           ])),
-                                  const SizedBox(height: 2.0),
+                                      const SizedBox(height: 2.0),
 
-                                  ///
-                                  (state.dataList[index].orgLandlineNo
-                                      .isNotEmpty &&
-                                      state.dataList[index].mobileNo
-                                          .isNotEmpty)
-                                      ? Text("Ph. " +
-                                      state.dataList[index]
-                                          .orgLandlineNo +
-                                      ", " +
-                                      state.dataList[index].mobileNo)
-                                      : Text("Ph. " +
-                                      state.dataList[index]
-                                          .orgLandlineNo +
-                                      state.dataList[index].mobileNo),
+                                      ///
+                                      (state.dataList[index].orgLandlineNo
+                                                  .isNotEmpty &&
+                                              state.dataList[index].mobileNo
+                                                  .isNotEmpty)
+                                          ? Text("Ph. " +
+                                              state.dataList[index]
+                                                  .orgLandlineNo +
+                                              ", " +
+                                              state.dataList[index].mobileNo)
+                                          : Text("Ph. " +
+                                              state.dataList[index]
+                                                  .orgLandlineNo +
+                                              state.dataList[index].mobileNo),
 
-                                  ///
-                                  const SizedBox(height: 5.0),
-                                  Text(
-                                      state
-                                          .dataList[index].orgContactPerson,
-                                      style: const TextStyle(
-                                        fontSize: 13.0,
-                                        color: Colors.green,
-                                        fontWeight: FontWeight.bold,
-                                      )),
-                                ]),
-                          ),
-                          Expanded(
-                            child: ElevatedButton(
-                              onPressed: () {
-                                ConfirmationAlert.confirmation(
-                                    text: "you want to send data ?",
-                                    context: context,
-                                    onCancel: () {
-                                      Navigator.pop(context);
-                                    },
-                                    onConfirm: () {
+                                      ///
+                                      const SizedBox(height: 5.0),
+                                      Text(
+                                          state
+                                              .dataList[index].orgContactPerson,
+                                          style: const TextStyle(
+                                            fontSize: 13.0,
+                                            color: Colors.green,
+                                            fontWeight: FontWeight.bold,
+                                          )),
+                                    ]),
+                              ),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    ConfirmationAlert.confirmation(
+                                        text: "you want to send data ?",
+                                        context: context,
+                                        onCancel: () {
+                                          Navigator.pop(context);
+                                        },
+                                        onConfirm: () {
                                           state.dataValue = state.filterList(
                                             orgName:
                                                 state.dataList[index].orgName,
                                           );
                                           state.onDataUpload(from: 'List');
                                         });
-                              },
-                              child: const Text(
-                                "UPLOAD",
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          )
-                        ]),
-                      ),
-                    ));
-              })
+                                  },
+                                  child: const Text(
+                                    "UPLOAD",
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              )
+                            ]),
+                          ),
+                        ));
+                  })
               : const NoDataWidget(),
         ),
         if (state.isLoading) LoadingScreen.loadingScreen()
